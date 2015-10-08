4e95c111744f420b69a3eead34d7429d60bf9ed9
Wataru MIYAGUNI <gonngo@gmail.com>
2015-10-08 09:28:48 +0900

    Bump version to 0.0.5

--- json-reformat.el
+++ json-reformat.el
@@ -2,7 +2,7 @@
 
 ;; Author: Wataru MIYAGUNI <gonngo@gmail.com>
 ;; URL: https://github.com/gongo/json-reformat
-;; Version: 0.0.4
+;; Version: 0.0.5
 ;; Keywords: json
 
 ;; Copyright (c) 2012 Wataru MIYAGUNI
